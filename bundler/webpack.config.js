704cf7096489692230c4e00d641bf67d336e72a5
Adam Král <adam.kral@moderntv.eu>
2017-04-15 18:43:23 +0200

    Initial commit, setting up webpack, first empty component

---
+++ bundler/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+module.exports = {
+    entry: '../main.jsx',
+    output: {
+        path: path.join('/var/www/html/colorPicker/bin'),
+        filename: 'app.bundle.js'
+    },
+    context: __dirname,
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react', 'stage-0']
+                }
+            }
+        ]
+    }
+}
---
+++ main.jsx
@@ -0,0 +1,5 @@
+import Container from './components/Container.jsx';
+ReactDOM.render(
+    <Container />,
+    document.getElementById('container')
+);
