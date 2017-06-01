dedbb53779dfddb7d29faac21e20dff495265aaa
adam <adam.kral@moderntv.eu>
2017-06-01 16:16:25 +0200

    fix editing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,17 @@ const root = document.getElementById('root');
 
 ReactDOM.render(
     <Main
+        cid={root.hasAttribute('data-configuration-id') ? root.getAttribute('data-configuration-id') : null}
+        colorNormal={root.hasAttribute('data-color-normal') ? root.getAttribute('data-color-normal') : null}
+        fontNormal={root.hasAttribute('data-font-normal') ? root.getAttribute('data-font-normal') : null}
+        colorFocused={root.hasAttribute('data-color-focused') ? root.getAttribute('data-color-focused') : null}
+        fontFocused={root.hasAttribute('data-font-focused') ? root.getAttribute('data-font-focused') : null}
+        colorDisabled={root.hasAttribute('data-color-disabled') ? root.getAttribute('data-color-disabled') : null}
+        fontDisabled={root.hasAttribute('data-font-disabled') ? root.getAttribute('data-font-disabled') : null}
+        configName={root.hasAttribute('data-config-name') ? root.getAttribute('data-config-name') : null}
+        image={root.hasAttribute('data-image') ? root.getAttribute('data-image') : null}
+        imageFormat={root.hasAttribute('data-image-format') ? root.getAttribute('data-image-format') : null}
+        mode={root.hasAttribute('data-mode') ? root.getAttribute('data-mode') : null}
     />,
     document.getElementById('root')
 );
